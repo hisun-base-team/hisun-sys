628ef8d646538c443331152b35d4dd63dd2a266a
rockwithyou <rockwithyou@126.com>
2018-04-20 19:10:43 +0800

    Modify TenantResource.

---
+++ src/main/java/com/hisun/saas/sys/tenant/resource/vo/TenantResourceVo.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright (c) 2018. Hunan Hisun Union Information Technology Co, Ltd. All rights reserved.
+ * http://www.hn-hisun.com
+ * 注意:本内容知识产权属于湖南海数互联信息技术有限公司所有,除非取得商业授权,否则不得用于商业目的.
+ */
+
+package com.hisun.saas.sys.tenant.resource.vo;
+
+
+public class TenantResourceVo {
+
+
+	private String id;
+	private String pId;
+	private String name;
+	private boolean open;
+	private String href;
+	
+	//resource entity中的对应属性，用于直接entity转vo在前台进行显示
+	private String resourceName;
+	private Integer resourceType;
+	private Integer status;
+	//private boolean isMenu;
+	private String url;
+	private Integer sort;
+	private String permission;
+	private String description;
+	private String resourceIcon;
+	private boolean chkDisabled;
+	private boolean checked;
+	
+	private boolean sysResource;
+	
+	public boolean isSysResource() {
+		return sysResource;
+	}
+
+	public void setSysResource(boolean sysResource) {
+		this.sysResource = sysResource;
+	}
+
+	public String getResourceIcon() {
+		return resourceIcon;
+	}
+
+	public void setResourceIcon(String resourceIcon) {
+		this.resourceIcon = resourceIcon;
+	}
+
+	public String getHref() {
+		return href;
+	}
+
+	public void setHref(String href) {
+		this.href = href;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getpId() {
+		return pId;
+	}
+
+	public void setpId(String pId) {
+		this.pId = pId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isOpen() {
+		return open;
+	}
+
+	public void setOpen(boolean open) {
+		this.open = open;
+	}
+
+	public String getResourceName() {
+		return resourceName;
+	}
+
+	public void setResourceName(String resourceName) {
+		this.resourceName = resourceName;
+	}
+
+	public Integer getResourceType() {
+		return resourceType;
+	}
+
+	public void setResourceType(Integer resourceType) {
+		this.resourceType = resourceType;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public Integer getSort() {
+		return sort;
+	}
+
+	public void setSort(Integer sort) {
+		this.sort = sort;
+	}
+
+	public String getPermission() {
+		return permission;
+	}
+
+	public void setPermission(String permission) {
+		this.permission = permission;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isChkDisabled() {
+		return chkDisabled;
+	}
+
+	public void setChkDisabled(boolean chkDisabled) {
+		this.chkDisabled = chkDisabled;
+	}
+
+	public boolean isChecked() {
+		return checked;
+	}
+
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+	
+}
